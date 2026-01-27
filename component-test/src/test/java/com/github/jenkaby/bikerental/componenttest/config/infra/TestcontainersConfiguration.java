af2df274c56c8801ba22fe79d913b560c8e4d18a
JenkaBY <evgen.kzumich@gmail.com>
2026-01-27 23:23:34 +0200

    fix: enable correctly liquibase for 'docker' profile

--- component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/config/infra/TestcontainersConfiguration.java
+++ component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/config/infra/TestcontainersConfiguration.java
@@ -4,19 +4,14 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 /**
  * Testcontainers configuration for component tests.
- * Assumes to run in CI/CD pipeline therefore liquibase is enabled.
  */
 @Profile("docker")
 @TestConfiguration(proxyBeanMethods = false)
-@TestPropertySource(properties = {
-        "spring.liquibase.enabled=true"
-})
 public class TestcontainersConfiguration {
 
     public static PostgreSQLContainer database;
