2318c7d6e610dda972cacf6be8b9798dc3d90993
JenkaBY <evgen.kzumich@gmail.com>
2026-01-27 23:23:34 +0200

    fix: enable correctly liquibase for 'docker' profile

--- component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/RunComponentTests.java
+++ component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/RunComponentTests.java
@@ -8,6 +8,8 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
@@ -21,12 +23,19 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 //@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@run")
 public class RunComponentTests {
 
-
     @SpringBootTest(classes = BikeRentalApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
     @CucumberContextConfiguration
     @Import(TestcontainersConfiguration.class)
     public static class ApplicationContextTestConfiguration {
 
+        @DynamicPropertySource
+        static void overrideProperties(DynamicPropertyRegistry registry) {
+            String activeProfiles = System.getProperty("spring.profiles.active", "");
+            if (activeProfiles.contains("docker")) {
+//              Assumes to run in CI/CD pipeline therefore liquibase is enabled.
+                registry.add("spring.liquibase.enabled", () -> true);
+            }
+        }
 
     }
 }
--- service/src/test/java/com/github/jenkaby/bikerental/BikeRentalApplicationTest.java
+++ service/src/test/java/com/github/jenkaby/bikerental/BikeRentalApplicationTest.java
@@ -4,12 +4,23 @@ import com.github.jenkaby.bikerental.support.TestcontainersConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class BikeRentalApplicationTest {
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        String activeProfiles = System.getProperty("spring.profiles.active", "");
+        if (activeProfiles.contains("docker")) {
+//              Assumes to run in CI/CD pipeline therefore liquibase is enabled.
+            registry.add("spring.liquibase.enabled", () -> true);
+        }
+    }
 
     @Test
     void contextLoads() {
