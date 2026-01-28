7f9c2008085736e13d0010b7278fcadf74f0f17e
JenkaBY <evgen.kzumich@gmail.com>
2026-01-28 13:37:14 +0200

    chore: read active profiles from env var

--- component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/RunComponentTests.java
+++ component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/RunComponentTests.java
@@ -32,6 +32,10 @@ public class RunComponentTests {
         @DynamicPropertySource
         static void overrideProperties(DynamicPropertyRegistry registry) {
             String activeProfiles = System.getProperty("spring.profiles.active", "");
+            String activeProfilesEnv = System.getenv("SPRING_PROFILES_ACTIVE");
+            if (activeProfilesEnv != null && !activeProfilesEnv.isBlank()) {
+                activeProfiles = activeProfiles + "," + activeProfilesEnv;
+            }
             if (activeProfiles.contains("docker")) {
 //              Assumes to run in CI/CD pipeline therefore liquibase is enabled.
                 log.info("!!!Docker profile is active - enabling liquibase migrations");
