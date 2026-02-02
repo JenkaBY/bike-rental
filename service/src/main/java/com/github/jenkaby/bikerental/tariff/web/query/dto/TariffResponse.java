b331f8fce85fb9c6eea30e3257105d129c702b5d
JenkaBY <evgen.kzumich@gmail.com>
2026-02-02 19:04:17 +0200

    feat: implement controllers

---
+++ service/src/main/java/com/github/jenkaby/bikerental/tariff/web/query/dto/TariffResponse.java
@@ -0,0 +1,24 @@
+package com.github.jenkaby.bikerental.tariff.web.query.dto;
+
+import com.github.jenkaby.bikerental.tariff.domain.model.TariffPeriod;
+import com.github.jenkaby.bikerental.tariff.domain.model.TariffStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record TariffResponse(
+        Long id,
+        String name,
+        String description,
+        String equipmentTypeSlug,
+        TariffPeriod period,
+        BigDecimal basePrice,
+        BigDecimal halfHourPrice,
+        BigDecimal hourPrice,
+        BigDecimal dayPrice,
+        BigDecimal hourDiscountedPrice,
+        LocalDate validFrom,
+        LocalDate validTo,
+        TariffStatus status
+) {
+}
---
+++ service/src/main/java/com/github/jenkaby/bikerental/tariff/web/query/mapper/TariffQueryMapper.java
@@ -0,0 +1,16 @@
+package com.github.jenkaby.bikerental.tariff.web.query.mapper;
+
+import com.github.jenkaby.bikerental.shared.mapper.MoneyMapper;
+import com.github.jenkaby.bikerental.tariff.domain.model.Tariff;
+import com.github.jenkaby.bikerental.tariff.web.query.dto.TariffResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = {MoneyMapper.class}
+)
+public interface TariffQueryMapper {
+
+    TariffResponse toResponse(Tariff tariff);
+}
