889211c2ee4f539a3b3651a0236d4217c48b18a4
JenkaBY <evgen.kuzmich@gmail.com>
2026-03-26 21:05:18 +0200

    fix: move JpaAuditing config to separate config

--- service/src/main/java/com/github/jenkaby/bikerental/BikeRentalApplication.java
+++ service/src/main/java/com/github/jenkaby/bikerental/BikeRentalApplication.java
@@ -3,11 +3,9 @@ package com.github.jenkaby.bikerental;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @Modulithic(sharedModules = "shared")
 @ConfigurationPropertiesScan
 public class BikeRentalApplication {
