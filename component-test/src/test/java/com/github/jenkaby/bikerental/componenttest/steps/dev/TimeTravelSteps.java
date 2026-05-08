ba6769a6764ce5fd715fb5031360ae7855f8b74b
JenkaBY <evgen.kuzmich@gmail.com>
2026-05-08 15:32:03 +0300

    chore: clean up

--- component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/steps/dev/TimeTravelSteps.java
+++ component-test/src/test/java/com/github/jenkaby/bikerental/componenttest/steps/dev/TimeTravelSteps.java
@@ -6,15 +6,9 @@ import com.github.jenkaby.bikerental.shared.web.TimeTravelController;
 import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
@@ -37,19 +31,6 @@ public class TimeTravelSteps {
     @Given("the time travel request instant is {string}")
     public void theTimeTravelRequestInstantIs(String instant) {
         scenarioContext.setRequestBody(new TimeTravelController.SetTimeRequest(Instant.parse(instant)));
-    }
-
-    @When("the time travel SSE snapshot is read")
-    public void theTimeTravelSseSnapshotIsRead() throws Exception {
-        var url = new URL("http://localhost:" + port + "/api/dev/time");
-        var conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestProperty("Accept", "text/event-stream");
-        conn.setReadTimeout(2000);
-        try (var reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-            var line = reader.readLine();
-            var json = line.replaceFirst("^data:\\s*", "");
-            scenarioContext.setResponse(ResponseEntity.ok(json));
-        }
     }
 
     @Then("the virtual clock instant is approximately the current time")
